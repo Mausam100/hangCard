effa376c17e102ed599b965bdf9ce1e6d37b5671
Mausam100 <smausam104@gmail.com>
2024-12-04 16:21:56 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card1 from "./Card1"
+import Card2 from "./Card2"
+import Card3 from "./Card3"
+const App = () => {
+  return (
+    <div className='h-screen flex w-full bg-black '>
+    <Card1/>
+    <Card2/>
+    <Card3/>
+    </div>
+  )
+}
+
+export default App
